1d43c2905124383f4fd708f195b2de91f7384fcb
Furuyama Takeshi <Furuyama.Takeshi0121@gmail.com>
2015-03-09 08:24:08 +0900

    textFieldタップでキーボードを表示するように修正

---
+++ SampleKeyboard/RoundLabel.swift
@@ -0,0 +1,21 @@
+//
+//  RoundLabel.swift
+//  SampleKeyboard
+//
+//  Created by Furuyama Takeshi on 2015/03/07.
+//  Copyright (c) 2015年 Furuyama Takeshi. All rights reserved.
+//
+
+import UIKit
+
+class RoundLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
